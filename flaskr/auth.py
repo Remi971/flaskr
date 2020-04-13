41049e85f55ae23a201e2f7005d253cd7d3ca1c6
Remi Bhagalou <remi.bha@gmail.com>
2020-04-13 14:38:42 +0200

    Fifth commit

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -38,13 +38,13 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
-        errror = None
+        error = None
         user = db.execute(
-            'SELECT * FROM user WHER username = ?', (username,)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
