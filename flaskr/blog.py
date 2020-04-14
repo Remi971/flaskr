1fefbd2ba61c9b978d2b5e3898e04845d60e2737
Remi Bhagalou <remi.bha@gmail.com>
2020-04-14 23:03:20 +0200

    The application is complete

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -66,11 +66,11 @@ def update(id):
 
     if request.method == 'POST':
         title = request.form['title']
-        body = request.form['title']
+        body = request.form['body']
         error = None
 
         if not title:
-            error = 'Title is required'
+            error = 'Title is required.'
 
         if error is not None:
             flash(error)
@@ -79,9 +79,18 @@ def update(id):
             db.execute(
                 'UPDATE post SET title = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id,)
+                (title, body, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
-        return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
